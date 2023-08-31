62c77636a5164b60546eb96bfe90db3e44052911
Natraj R <135331209+Natraj-10@users.noreply.github.com>
2023-08-31 19:40:42 +0530

    Update script.js
    Code for timer

---
+++ script.js
@@ -0,0 +1,21 @@
+const targetDate = new Date("2023-09-04T07:30:00");
+const timerElement = document.getElementById("timer");
+
+function updateCountdown() {
+    	const now = new Date().getTime();
+    	const timeLeft = targetDate - now;
+
+    	const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    	const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    	const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    	const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+	const adays = days.toString().padStart(2, '0');
+	const ahours = hours.toString().padStart(2, '0');
+	const aminutes = minutes.toString().padStart(2, '0');
+	const aseconds = seconds.toString().padStart(2, '0');
+
+    	timerElement.innerHTML = `${adays} : ${ahours} : ${aminutes} : ${aseconds} `;
+}
+
+setInterval(updateCountdown, 1000);
